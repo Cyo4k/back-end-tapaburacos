081682d9e5b5b0a6fd16d76e39b11bf6cc0de01c
João <jpauloxpo@gmail.com>
2024-11-26 13:45:25 -0300

    test

--- src/index.js
+++ src/index.js
@@ -4,13 +4,20 @@ const mongoose = require("mongoose");
 const dbUser = process.env.DB_USER;
 const dbPassword = process.env.DB_PASS;
 const conect = `mongodb+srv://${dbUser}:${dbPassword}@tapaburacos.zwl1r.mongodb.net/?retryWrites=true&w=majority&appName=tapaBuracos`;
+
 const cors = require("cors");
+const corsconfig = {
+    origin: "*",
+    credential: true,
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+}
 
 const authRoutes = require("./routes/authRoutes");
 const reportRoutes = require("./routes/reportRoutes");
 const app = express();
 const PORT = process.env.PORT || 3000;
-app.use(cors());
+app.options("", cors(corsconfig))
+app.use(cors(corsconfig));
 app.use(express.json());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
